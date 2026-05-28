352400a19c5b3450301f488a833e504ab4c364ed
JIMENEZ <JIMENEZ@LAPTOP-SEBAS>
2026-05-28 02:07:15 -0500

    SEBASTIAN MANUEL FIGUEROA JIMENEZ

--- JavaApplication13/src/vista/Main.java
+++ JavaApplication13/src/vista/Main.java
@@ -4,29 +4,46 @@
  */
 package vista;
 
+import controlador.ContratoCorporativoBuilder;
+import controlador.DirectorDocumento;
+import controlador.DocumentoBuilder;
 import modelo.ConfigurationManager;
 import modelo.DocumentoLegal;
-import controlador.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // SINGLETON
+        // ==========================
+        // SINGLETON (CONFIGURACIÓN GLOBAL)
+        // ==========================
         ConfigurationManager config = ConfigurationManager.getInstancia();
 
-        System.out.println("---- CONFIGURACIÓN GLOBAL ----");
-        System.out.println(config.getUrlBaseDatos());
+        System.out.println("==================================");
+        System.out.println(" SISTEMA DE DOCUMENTOS EMPRESARIALES ");
+        System.out.println("==================================");
 
-        System.out.println("\n\n");
+        System.out.println("\n--- CONFIGURACION GLOBAL ---");
+        System.out.println("Base de Datos: " + config.getUrlBaseDatos());
+        System.out.println("Servicio Web : " + config.getUrlServicioWeb());
+        System.out.println("Sistema      : " + config.getSistemaOperativo());
 
-        // BUILDER
+        System.out.println("\n==================================\n");
+
+        // ==========================
+        // BUILDER (CONTRATO)
+        // ==========================
         DocumentoBuilder builder = new ContratoCorporativoBuilder();
+
         DirectorDocumento director = new DirectorDocumento(builder);
 
+        // Construcción del documento paso a paso
         director.construirDocumentoCompleto();
 
-        DocumentoLegal doc = builder.obtenerDocumento();
-        doc.mostrarDocumento();
+        // Resultado final
+        DocumentoLegal contrato = builder.obtenerDocumento();
+
+        System.out.println("========= CONTRATO GENERADO =========\n");
+        contrato.mostrarDocumento();
     }
 }
